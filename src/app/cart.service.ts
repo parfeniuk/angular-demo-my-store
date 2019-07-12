b49865d305a9fba1955a2b11b1fbde268ef35240
parfeniuk <o.parfeniuk@gmail.com>
2019-07-12 15:24:09 +0300

    Added Cart Page

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn:'root'
+  providedIn: 'root'
 })
 export class CartService {
-  items=[];
-  
-  addToCart(product){
+  items = [];
+
+  addToCart(product) {
     this.items.push(product);
   }
-  getItems(){
-    return this.items;
-  }
-  clearCart(){
-    this.items=[];
-    return this.items;
-  }
-  constructor() { }
 
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
